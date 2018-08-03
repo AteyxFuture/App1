d69162e84156ea4d3d29e9652bcb2f73094ff907
Atey <vojtech.horky@outlook.com>
2018-08-03 13:19:09 +0200

    Add health and a few methods for it

--- Creature.py
+++ Creature.py
@@ -3,19 +3,16 @@ from Item import *
 
 class Creature:
 
-    def __init__(self, name, age, weight, exp, inventory):
+    def __init__(self, name, age, weight, exp, inventory, health):
         self.name = name
         self.age = int(age)
         self.weight = int(weight)
         self.exp = int(exp)
         self.inventory = inventory
-        self.property = [self.age, self.weight, self.exp, self.inventory]
+        self.health = health
 
     def get_name(self):
         return self.name
-
-    def get_values(self):
-        return self.property
 
     def take_item(self, item):
         self.inventory.append(item)
@@ -24,8 +21,13 @@ class Creature:
         self.inventory.remove(item)
 
     def use_item(self, item):
-        item.use()
+        item.use(self)
 
+    def heal(self, severity):
+        self.health += severity
+
+    def hurt(self, severity):
+        self.health -= severity
 
 path = ("Creature_list.txt")
 with open(path) as file:
@@ -35,9 +37,9 @@ creatures = text.split('\n')
 for i, e in enumerate(creatures):
     creatures[i] = e.split('\t')
 
-def make_creature(name, age, weight, exp, inventory):
-    creature = Creature(name, age, weight, exp, inventory)
+def make_creature(name, age, weight, exp, inventory, health):
+    creature = Creature(name, age, weight, exp, inventory, health)
     return creature
 
 for i, e in enumerate(creatures):
-    creatures[i] = make_creature(e[0], e[1], e[2], e[3], eval(e[4]))
+    creatures[i] = make_creature(e[0], e[1], e[2], e[3], eval(e[4]), e[5])
